ec0bf0975ae09c5890a88e21899744654545650b
Glaudiston Gomes da Silva <glaudiston.silva@synchro.com.br>
2020-10-14 10:21:48 -0300

    bash PoC file creating a valid elf

---
+++ make-elf.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+# see man elf - /usr/include/elf.h has all information including enums
+# https://www.airs.com/blog/archives/38
+# http://www.sco.com/developers/gabi/latest/ch4.eheader.html
+#
+# System Interrupt call table for 64bit linux:
+# http://www.cpu2.net/linuxabi.html
+#
+# x86 Instruction set
+# https://en.wikipedia.org/wiki/X86_instruction_listings#Original_8086.2F8088_instructions
+#
+
+EI_MAG3="\x7fELF" # ELF Index Magic 4 bytes, positions form 0 to 3
+EI_CLASS="\x00"
+[ "$(uname -m)" == "x86" ] && EI_CLASS="\x01"
+[ "$(uname -m)" == "x86_64" ] && EI_CLASS="\x02"
+
+EI_DATA="$(dd if=/bin/touch bs=7 count=1 status=none | tail -c 1)" # get endianness from touch binary
+EI_VERSION="\x01"	# 1 (current)
+EI_OSABI="\x00"		# Operation System Applications Binary Interface (UNIX - System V)
+EI_ABIVERSION="\x00"
+EI_PAD="\x00\x00\x00\x00\x00\x00\x00\x00" # reserved non used pad bytes
+EI_ETYPE="\x03\x00";	# DYN (Shared object file) code 3 in little endian
+EI_MACHINE="\x3e\x00";	# EM_X86_64 in little endian
+EI_MACHINE_VERSION="\x01\x00\x00\x00" # 1 in little endian
+EI_ENTRY="\x40\x00\x00\x00\x00\x00\x00\x00";	# TODO: program code entry point uint64_t
+EI_PHOFF="\x40\x00\x00\x00\x00\x00\x00\x00";	# TODO: program header size
+EI_SHOFF="\x00\x00\x00\x00\x00\x00\x00\x00";	# TODO: section header offset in bytes
+EI_FLAGS="\x00\x00\x00\x00";	# uint32_t
+EI_EHSIZE="\x40\x00";	# TODO: elf header size in bytes
+EI_PHENTSIZE="\x38\x00" # TODO: program header entry size (sizeof(Elf64_Phdr))
+EI_PHNUM="\x01\x00"; 	# TODO number of program header entries
+EI_SHENTSIZE="\x00\x00";	# TODO section header size in bytes
+EI_SHNUM="\x00\x00"; 	# TODO section header count
+EI_SHSTRNDX="\x00\x00"; # TODO section header table index of entry of section name string table
+
+
+SECTION_ELF_HEADER="${EI_MAG3}${EI_CLASS}${EI_DATA}${EI_VERSION}${EI_OSABI}${EI_PAD}"; # 16 bytes
+SECTION_ELF_HEADER="${SECTION_ELF_HEADER}${EI_ETYPE}${EI_MACHINE}${EI_MACHINE_VERSION}${EI_ENTRY}";
+SECTION_ELF_HEADER="${SECTION_ELF_HEADER}${EI_PHOFF}${EI_SHOFF}"
+SECTION_ELF_HEADER="${SECTION_ELF_HEADER}${EI_FLAGS}${EI_EHSIZE}${EI_PHENTSIZE}${EI_PHNUM}${EI_SHENTSIZE}${EI_SHNUM}${EI_SHSTRNDX}";
+
+SH_NAME="\x01\x00\x00\x00"; # Section name (String table index) uint32_t
+SH_TYPE="\x00\x00\x00\x00"; # Section type
+SH_FLAGS="\x00\x00\x00\x00\x00\x00\x00\x00" # uint64_t
+
+SECTION_HEADERS="${SH_NAME}"; # Elf64_Shdr
+
+# https://www.airs.com/blog/archives/45
+PH_TYPE="\x01\x00\x00\x00"	# Elf64_Word p_type 4;
+PH_FLAGS="\x00\x00\x00\x00"	# Elf64_Word p_flags 4;
+PH_OFFSET="\x00\x00\x00\x00\x00\x00\x00\x00"	# Elf64_Off p_offset 8;
+PH_VADDR="\x00\x00\x00\x00\x00\x00\x00\x00"	# Elf64_Addr p_vaddr 8;
+PH_PADDR="\x00\x00\x00\x00\x00\x00\x00\x00"	# Elf64_Addr p_paddr 8;
+PH_FILESZ="\x00\x00\x00\x00\x00\x00\x00\x00"	# Elf64_Xword p_filesz 8;
+PH_MEMSZ="\x00\x00\x00\x00\x00\x00\x00\x00"	# Elf64_Xword p_memsz 8;
+PH_ALIGN="\x00\x00\x00\x00\x00\x00\x00\x00"	# Elf64_Xword p_align 8;
+PROGRAM_HEADERS="${PH_TYPE}";
+PROGRAM_HEADERS="${PROGRAM_HEADERS}${PH_FLAGS}";
+PROGRAM_HEADERS="${PROGRAM_HEADERS}${PH_OFFSET}";
+PROGRAM_HEADERS="${PROGRAM_HEADERS}${PH_VADDR}";
+PROGRAM_HEADERS="${PROGRAM_HEADERS}${PH_PADDR}";
+PROGRAM_HEADERS="${PROGRAM_HEADERS}${PH_FILESZ}";
+PROGRAM_HEADERS="${PROGRAM_HEADERS}${PH_MEMSZ}";
+PROGRAM_HEADERS="${PROGRAM_HEADERS}${PH_ALIGN}"; # Elf64_Phdr[]
+SECTION_ELF_DATA="${PROGRAM_HEADERS}${SECTION_HEADERS}";
+echo -ne "${SECTION_ELF_HEADER}${SECTION_ELF_DATA}" > elf
