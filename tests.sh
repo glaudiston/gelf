746939522f07871ef3334623940c947be724be3c
Glaudiston Gomes da Silva <glaudistong@gmail.com>
2024-02-14 11:28:36 -0300

    adding tests

--- tests.sh
+++ tests.sh
@@ -21,7 +21,7 @@ with no error:	0
 exit	with no error
 EOF
 	o=$(run_test);
-	expect $? 0 "hello world";
+	expect $? 0 "hello world" "$o";
 }
 
 test_arg_count(){
@@ -33,7 +33,7 @@ with no error:	0
 exit	with no error
 EOF
 	o=$(run_test abc def|xxd --ps);
-	expect $? 0 03
+	expect $? 0 03 "$o"
 }
 
 test_write_out_arg(){
@@ -45,8 +45,52 @@ with no error:	0
 exit	with no error
 EOF
 	o=$(run_test abc);
-	expect $? 0 abc;
+	expect $? 0 abc "$o";
 }
 
+
+# a known bug is that the write autodetect the size by the \x00 byte;
+# so the read will stop at the first \x00 byte
+test_read_text_file(){
+	head /dev/random | xxd > .tmp.file;
+	chk=$(md5sum .tmp.file | cut -d " " -f1);
+	compile_test <<EOF
+file name:	./.tmp.file
+f:<=	file name
+stdout:	1
+write	stdout	f
+with no error:	0
+exit	with no error
+EOF
+	o=$(run_test | md5sum | cut -d " " -f1);
+	expect $? 0 $chk "$o";
+}
+
+test_exec(){
 # this should be the last line on file:
+	compile_test <<EOF
+cmd:	/usr/bin/whoami
+!	cmd
+success:	0
+exit	success
+EOF
+	chk=$(whoami)
+	o=$(run_test)
+	expect $? 0 $chk "$o"
+}
+
+test_concat_static_symbols(){
+	compile_test <<EOF
+a:	abc
+b:	def
+c:|	a	b
+out:	1
+write	out	c
+d:	0
+exit	d
+EOF
+	o=$(run_test)
+	expect $? 0 "abcdef" "$o"
+}
+
 . ./test_suite.sh
